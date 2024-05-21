d7b9f1de5812351d7860c69046bf6655c0e2e933
MaksymPyvovarov <pivovarovjr@gmail.com>
2024-05-21 18:10:36 +0200

    2024-05-21 MP

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from app import app
 from app import utils
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, send_file
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+from io import BytesIO
+import xlsxwriter
 import os
 import json
 from matplotlib import pyplot as plt
@@ -58,7 +60,7 @@ def extract():
                     opinions_count = len(opinions)
                     pros_count = opinions.pros.apply(lambda p: None if not p else p).count()
                     cons_count = opinions.cons.apply(lambda c: None if not c else c).count()
-                    average_score = opinions.score.mean()*5
+                    average_score = round(opinions.score.mean()*5, 2)
                     score_distribution = opinions.score.value_counts().reindex(np.arange(0, 5.5, 0.5), fill_value = 0)
                     recommendation_distribution = opinions.recommendation.value_counts(dropna=False).reindex([True, False, np.nan], fill_value = 0)
                     product = {
@@ -120,3 +122,23 @@ def author():
 @app.route('/product/<product_id>')
 def product(product_id):
     return render_template("product.html", product_id = product_id)
+
+@app.route('/download/json/<product_id>')
+def download_json(product_id):
+    return send_file(f"opinions/{product_id}.json", mimetype='text/json', download_name=f'{product_id}.json', as_attachment=True)
+
+@app.route('/download/csv/<product_id>')
+def download_csv(product_id):
+    opinions = pd.read_json(f"app/opinions/{product_id}.json")
+    response_stream = BytesIO(opinions.to_csv().encode())
+    return send_file(response_stream, mimetype='text/csv', download_name=f'{product_id}.csv', as_attachment=True)
+
+@app.route('/download/xlsx/<product_id>')
+def download_xlsx(product_id):
+    opinions = pd.read_json(f"app/opinions/{product_id}.json")
+    buffer = BytesIO()
+    with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
+        opinions.to_excel(writer)
+    buffer.seek(0)
+    return send_file(buffer, mimetype='application/vnd.ms-excel', download_name=f'{product_id}.xlsx', as_attachment=True)
+    
