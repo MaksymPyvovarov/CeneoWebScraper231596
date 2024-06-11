738e8c49940f414ca2bc9b61474df96dac7f6255
MaksymPyvovarov <pivovarovjr@gmail.com>
2024-06-11 16:38:45 +0200

    06-11-2024 MP

--- app/routes.py
+++ app/routes.py
@@ -51,51 +51,59 @@ def extract():
                             url = "https://www.ceneo.pl/" + utils.extract(page_dom, "a.pagination__next", "href")
                         except TypeError:
                             url = None
-                    if not os.path.exists("app/opinions"):
-                        os.mkdir("app/opinions")
-                    jf = open(f"app/opinions/{product_id}.json", "w", encoding="UTF-8")
-                    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
-                    jf.close()
-                    opinions = pd.DataFrame.from_dict(all_opinions)
-                    opinions_count = len(opinions)
-                    pros_count = opinions.pros.apply(lambda p: None if not p else p).count()
-                    cons_count = opinions.cons.apply(lambda c: None if not c else c).count()
-                    average_score = round(opinions.score.mean()*5, 2)
-                    score_distribution = opinions.score.value_counts().reindex(np.arange(0, 5.5, 0.5), fill_value = 0)
-                    recommendation_distribution = opinions.recommendation.value_counts(dropna=False).reindex([True, False, np.nan], fill_value = 0)
-                    product = {
-                        'product_id' : product_id,
-                        'product_name' : product_name,
-                        'opinions_count': int(opinions_count),
-                        'pros_count': int(pros_count),
-                        'cons_count': int(cons_count),
-                        'average_score' : average_score,
-                        'score_distribution': score_distribution.to_dict(),
-                        'recommendation_distribution': recommendation_distribution.to_dict()
-                    }
-                    if not os.path.exists("app/products"):
-                        os.mkdir("app/products")
-                    jf = open(f"app/products/{product_id}.json", "w", encoding="UTF-8")
-                    json.dump(product, jf, indent=4, ensure_ascii=False)
-                    jf.close()
-                    if not os.path.exists("app/charts"):
-                        os.mkdir("app/charts")
-                    fig, ax = plt.subplots()
-                    score_distribution.plot.bar(color = "turquoise")
-                    plt.xlabel("Number of stars")
-                    plt.xticks(rotation = 0)
-                    plt.ylabel("Number of opinions")
-                    plt.title(f"Score histogram for {product_id} product")
-                    ax.bar_label(ax.containers[0], label_type="edge", fmt= lambda l: int(l) if l else "")
-                    plt.savefig(f"app/charts/{product_id}_score.png")
-                    recommendation_distribution.plot.pie(
-                        labels = ["Recommend", "Not recommend", "Indifferent"],
-                        label = "",
-                        colors = ["forestgreen", "crimson", "silver"],
-                        autopct = lambda l: "{:1.1f}%".format(1) if l else ""
-                    )
-                    plt.title(f"Recommendation shares for {product_id} product")
-                    plt.savefig(f"app/charts/{product_id}_recommendation.png")
+                if not os.path.exists("app/opinions"):
+                    os.mkdir("app/opinions")
+                jf = open(f"app/opinions/{product_id}.json", "w", encoding="UTF-8")
+                json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+                jf.close()
+                opinions = pd.DataFrame.from_dict(all_opinions)
+                MAX_SCORE = 5
+                opinions.score = opinions.score.apply(lambda v: round(v * MAX_SCORE,1))
+                opinions_count = opinions.index.size
+                pros_count = opinions.pros.apply(lambda p: None if not p else p).count()
+                cons_count = opinions.cons.apply(lambda p: None if not p else p).count()
+                average_score = round(opinions.score.mean(), 2)
+                score_distribution = opinions.score.value_counts().reindex(np.arange(0, 5.5, 0.5), fill_value = 0)
+                recommendation_distribution = opinions.recommendation.value_counts(dropna=False).reindex([True, False, np.nan], fill_value=0)
+                product = {
+                    'product_id' : product_id,
+                    'product_name' : product_name,
+                    'opinions_count': int(opinions_count),
+                    'pros_count': int(pros_count),
+                    'cons_count': int(cons_count),
+                    'average_score' : average_score,
+                    'score_distribution': score_distribution.to_dict(),
+                    'recommendation_distribution': recommendation_distribution.to_dict()
+                }
+                if not os.path.exists("app/products"):
+                    os.mkdir("app/products")
+                jf = open(f"app/products/{product_id}.json", "w", encoding="UTF-8")
+                json.dump(product, jf, indent=4, ensure_ascii=False)
+                jf.close()
+                if not os.path.exists("app/static"):
+                    os.mkdir("app/static")
+                if not os.path.exists("app/static/charts"):
+                    os.mkdir("app/static/charts")
+                fig, ax = plt.subplots()
+                score_distribution.plot.bar(color="turquoise", ax=ax)
+                plt.xlabel("Number of stars")
+                plt.ylabel("Number of opinions")
+                plt.title(f"Score histogram for {product_id} product")
+                plt.xticks(rotation=0)
+                ax.bar_label(ax.containers[0], label_type='edge', fmt=lambda l: f'{int(l)}' if l else '')
+                plt.savefig(f"app/static/charts/{product_id}_score.png")
+                plt.close(fig)
+
+                fig, ax = plt.subplots()
+                recommendation_distribution.plot.pie(
+                    labels=["Recommend", "Not recommend", "Indifferent"],
+                    label='',
+                    colors=["forestgreen", "crimson", "silver"],
+                    autopct=lambda p: f'{p:.1f}%' if p > 0 else ''
+                )
+                plt.title(f"Recommendation shares for {product_id} product")
+                plt.savefig(f"app/static/charts/{product_id}_recommendation.png")
+                plt.close(fig)
                 return redirect(url_for('product', product_id=product_id))
             return render_template("extract.html", error = "Product has no opinions")
         return render_template("extract.html", error = "Product does not exist")
